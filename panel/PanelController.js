91ca3a34cdbd3cab19b49c11b99fa1f3b636719e
jonhsilvaprojetos <jonhsilvaprojetos@gmail.com>
2020-04-26 20:42:44 -0300

    Ajustes iniciais

--- panel/PanelController.js
+++ panel/PanelController.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/schedule', (req, res)=>{
+    res.render('week', {className: 'schedule'});
+});
+
 router.get('/', (req, res)=>{
-    res.render('schedule', {className: 'schedule'});
+    res.render('index', {className: 'home'});
 });
 
 module.exports = router;
